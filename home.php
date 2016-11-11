64aed6f295f68999d39f8f219dd6102081d28500
ORAARTHORNSOMBAT <OA3CJ@VIRGINIA.EDU>
2016-11-10 23:32:46 -0500

    Login, logout, and error messages

--- home.php
+++ home.php
@@ -1,77 +1,88 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-  <title>Home</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-  integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-  <link rel="Stylesheet" href="style.css">
-</head>
-<body>
-  <div class="container" id="page-wrap">
+<?php
+session_start();
+if(!$_SESSION['computing_id'])  
+{  
+    header("Location: login.php");//redirect to login page to secure the welcome page without login access.  
+  }  
+  ?>
 
-   <div class="header">
-    <div style="float:left">
-      <img id="logo" src="logo.png" width="100" height="100">
-    </div>
-    <div style="float:clear; display:inline-block; margin:1%;">
-      <h3><i> aSISt </i></h3>
-    </div>
-  </div>
+  <!DOCTYPE HTML>
+  <html>
+  <head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+    integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="Stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="container" id="page-wrap">
 
-
-  <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a href="/asist/home.php" class="navbar-brand">aSISt</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                  <li><a href="/asist/home.php">Home </a></li>
-                  <li><a href="/asist/searchResult.php">Course Search </a></li>
-                  <li><a href="/asist/classSchedule.php">Class Schedule </a></li>
-                  <li><a href="/asist/personalInfo.php">Personal Information </a></li>
-              </ul>
-              <ul class="nav navbar-nav navbar-right">
-                  <li><a href="/asist/home.php">Signed in as ~~name~~</a></li>
-                  <li><a href="/asist/login.php">Logout</a></li>
-              </ul>
-          </div>
+     <div class="header">
+      <div style="float:left">
+        <img id="logo" src="logo.png" width="100" height="100">
       </div>
-  </nav>
+      <div style="float:clear; display:inline-block; margin:1%;">
+        <h3><i> aSISt </i></h3>
+      </div>
+    </div>
 
-<?php 
-
+    <?php
   //--------------------------------------------------------------------------
   // Fetch data from mysql database
   //--------------------------------------------------------------------------
     $db = new mysqli('localhost', 'username', 'password', 'asist');
-      if ($db->connect_error):
-         die ("Could not connect to db: " . $db->connect_error);
-      endif;
+    if ($db->connect_error):
+     die ("Could not connect to db: " . $db->connect_error);
+   endif;
 
-      $query = "select * from STUDENT";
-      $result = $db->query($query);
+   function getStudent($db){
+    $query = "select * from student";
+    $result = $db->query($query);
+    if ($result->num_rows>0){
       $row = $result->fetch_array();
+      // $computing_id = $row["computing_id"];
+      // echo $computing_id;
+      return $row;
+    } 
+  }
 
-      $computing_id = $row["computing_id"];
-
-      echo "$computing_id";
   ?>
+
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="/asist/home.php" class="navbar-brand">aSISt</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="/asist/home.php">Home </a></li>
+        <li><a href="/asist/searchResult.php">Course Search </a></li>
+        <li><a href="/asist/classSchedule.php">Class Schedule </a></li>
+        <li><a href="/asist/personalInfo.php">Personal Information </a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/asist/home.php"><?php echo $_SESSION['computing_id'];?></a></li>
+        <li><a href="/asist/logout.php">Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
 
 <div id="main-buttons">
   <button id="course-search" class="big-button" onClick="window.location = '/asist/searchResult.php'">Course Search</button><br>
--- login.php
+++ login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -10,29 +14,127 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
   integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
   <link rel="Stylesheet" href="style.css">
+
   <title>aSISt</title>
 </head>
 
 <body>
-  <form>
-    <div class="container" style="text-align:center">
-      <div class = "login-form">
+ 
+  <div class="container" style="text-align:center">
+    <div class = "login-form">
+      <form class = "form-signin" role = "form" 
+      action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
+      ?>" method = "post">
+      <legend><h2>Welcome</h2></legend><br/>
+      <input type = "text" class = "form-control" 
+      name = "computing_id" placeholder = "Computing ID" 
+      required autofocus></br>
+      <input type = "password" class = "form-control"
+      name = "password" placeholder = "Password" required>
+      <br/>
+      <button class = "btn btn-info btn-lg btn-primary btn-block" type = "submit" 
+      name = "login">Login</button>
+    </form>
+  </div>
+</div>
 
-        <legend><h2>Welcome</h2></legend><br/>
-        
-        <div class="form-group">
-          <input type="text" name="computingid" placeholder="Computing ID" required class="form-control" />
-        </div>
-        <div class="form-group">
-          <input type="password" name="password" placeholder="Password" required class="form-control" />
-        </div>
-
-        
-        <button type="button" class="btn btn-info btn-primary btn-block" style="padding:1%">Log In</button>
+<!-- Modal 1 -->
+<div class="modal fade" id="errorIDModal" role="dialog">
+  <div class="modal-dialog">
+    
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Error!</h4>
       </div>
-
+      <div class="modal-body">
+        <p>Invalid computing ID</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Try Again</button>
+      </div>
     </div>
-  </form>
+    
+  </div>
+</div>
+
+<!-- Modal 1 -->
+<div class="modal fade" id="errorPasswordModal" role="dialog">
+  <div class="modal-dialog">
+    
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Error!</h4>
+      </div>
+      <div class="modal-body">
+        <p>Invalid password</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Try Again</button>
+      </div>
+    </div>
+    
+  </div>
+</div>
+
 </body>
 </html>
 
+<?php
+
+
+$db = new mysqli('localhost', 'username', 'password', 'asist');
+if ($db->connect_error):
+ die ("Could not connect to db: " . $db->connect_error);
+endif;
+
+$query = "select * from student";
+$result = $db->query($query);
+if ($result->num_rows>0){
+  $row = $result->fetch_array();
+  $computing_id = $row["computing_id"];
+  $password = $row["password"];
+}
+
+
+if(isset($_POST['login']))  
+{  
+
+ $computing_id=$_POST['computing_id'];  
+ $password=$_POST['password'];  
+ 
+ $check_user="select * from student WHERE computing_id='$computing_id'and password='$password'";
+ 
+ $run=mysqli_query($db,$check_user);  
+ 
+ if(mysqli_num_rows($run))  
+ {  
+  echo "<script>window.open('home.php','_self')</script>";  
+  
+  $_SESSION['computing_id']=$computing_id; 
+  
+}  
+else  
+{ 
+  $check_ID="select * from student WHERE computing_id='$computing_id'";
+  $check_password = "select * from student WHERE password='$password'";
+  $result_ID = $db->query($check_ID);
+  $result_password = $db->query($check_password);
+  if ($result_ID->num_rows==0)
+  {  
+    ?>
+    <script>$('#errorIDModal').modal('show');</script>
+    
+    <?php
+  } else if ($result_password->num_rows==0){
+    ?>
+    <script>$('#errorPasswordModal').modal('show');</script>
+    <?php
+  }
+      // echo "<script>alert('Email or password is incorrect!')</script>"; 
+}  
+}  
+?>
